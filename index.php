8b560cd0c3574b41512e6a0bcde0b00a96c61760
ngothanhtan <oh.sick.cf@gmail.com>
2019-09-05 14:28:00 +0700

    taolayout

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php include("tentrangcanduavao.php"); ?>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css"href="css.css">
+</head>
+<body>
+<div class="container">
+    <header><?php  include "header.php";  ?></header>
+    <nav><?php include "nav.php";  ?></nav>
+    <article> <?php include "content.php"; ?></article>
+    <footer><?php include "footer.php";?></footer>
+</div>
+</body>
+</html>
